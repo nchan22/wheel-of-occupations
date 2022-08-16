4df159d66951ebee5441fadd56659d3c797ee7f1
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-08-16 18:18:30 -0400

    Fixed typo error

--- src/components/JeopardyBoard/index.js
+++ src/components/JeopardyBoard/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 
-function JeopardyBoard {}
+function JeopardyBoard() {}
 
-export default JeopardyBoard
+export default JeopardyBoard;
--- src/components/SpinningWheel/index.js
+++ src/components/SpinningWheel/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import "./style.css"
+import "./style.css";
 import { Container } from "react-dom";
 
-function SpinningWheel {
-    return(
-        <Container>
-            <Button id="spin">Spin</Button>
-        </Container>
-    )
-           
+function SpinningWheel() {
+  return (
+    <Container>
+      <Button id="spin">Spin</Button>
+    </Container>
+  );
 }
 
-export default SpinningWheel
+export default SpinningWheel;
