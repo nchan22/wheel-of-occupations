62ad9ef69d7c9c286e4fd223471be846350eb4fa
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-08-16 17:37:47 -0400

    Added spin button

--- src/components/SpinningWheel/index.js
+++ src/components/SpinningWheel/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import "./style.css"
 
-function SpinningWheel {}
+function SpinningWheel {
+    return(
+        <Button id="spin">Spin</Button>
+    )
+           
+}
 
 export default SpinningWheel
